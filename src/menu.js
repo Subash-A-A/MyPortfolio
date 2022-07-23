720a3bf9b57f92014e5a972e17f6b9aa9ced9c13
Subash <it.subashaa@gmail.com>
2022-07-23 22:54:24 +0530

    scroll lock and circleNav

--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,37 @@
-const toggle = document.querySelector(".toggle__button");
-const menu = document.querySelector(".mobile__menu");
+const toggle = document.querySelector(".toggle__button")
+const menu = document.querySelector(".mobile__menu")
+
+const navCircleMenu = document.querySelector(
+  ".nav__circles__container"
+).childNodes
+
+navCircleMenu.forEach((circle) => {
+  circle.addEventListener("click", () => {
+    removeActive()
+    circle.classList.add("active")
+  })
+})
 
 toggle.addEventListener("click", () => {
-  const check = menu.getAttribute("aria-expanded");
-  const button_check = toggle.getAttribute("data-button-clicked");
+  const check = menu.getAttribute("aria-expanded")
+  const button_check = toggle.getAttribute("data-button-clicked")
   if (check === "false" && button_check === "false") {
-    menu.setAttribute("aria-expanded", true);
-    toggle.setAttribute("data-button-clicked", true);
-    menu.classList.add("menu__show");
+    menu.setAttribute("aria-expanded", true)
+    toggle.setAttribute("data-button-clicked", true)
+    menu.classList.add("menu__show")
   } else if (check === "true" && button_check === "true") {
-    menu.setAttribute("aria-expanded", false);
-    toggle.setAttribute("data-button-clicked", false);
-    menu.classList.remove("menu__show");
+    menu.setAttribute("aria-expanded", false)
+    toggle.setAttribute("data-button-clicked", false)
+    menu.classList.remove("menu__show")
   }
-});
+})
+
+function MoveToPage(url) {
+  window.location.href = "#" + url
+}
+
+function removeActive() {
+  navCircleMenu.forEach((circle) => {
+    circle.classList.remove("active")
+  })
+}
--- src/target.js
+++ src/target.js
@@ -10,11 +10,20 @@
 
 const target__box = document.querySelector(".target__box").childNodes
 
+function disableScroll() {
+  window.addEventListener("DOMMouseScroll", preventDefault, false) // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt) // modern desktop
+  window.addEventListener("touchmove", preventDefault, wheelOpt) // mobile
+  window.addEventListener("keydown", preventDefaultForScrollKeys, false)
+}
+
 target__box.forEach((target) => {
   target.addEventListener("click", () => {
+    target.classList.remove("target__hover")
     target.classList.add("fall")
     setTimeout(() => {
       target.classList.remove("fall")
+      target.classList.add("target__hover")
     }, 3000)
   })
 })
